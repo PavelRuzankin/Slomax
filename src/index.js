fc7f9e0c051232835cb663d8cfe5bd40a06186bb
PavelRuzankin <61935146+PavelRuzankin@users.noreply.github.com>
2022-12-29 11:03:29 +0300

    fix slomax

--- src/index.js
+++ src/index.js
@@ -1,17 +1,130 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Context = React.createContext(null) 
+
+const createStore = (reducer, initialState) => {
+  let currentState = initialState
+  let listeners = []
+
+  const getState = () => currentState
+  
+  const dispatch = action => {
+    currentState = reducer(currentState, action)
+    listeners.forEach(listener => listener())
+  }
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+
+    return () => {
+      const intex = listeners.indexOf(listener)
+      listeners.splice(intex, 1)
+    }
+  }
+
+  return { getState, dispatch, subscribe }
+}
+
+const useSelector = (selector, isEqual) => {
+  const { store } = React.useContext(Context)
+
+  const [selectedStore, setSelectedStore] = useState(selector(store.getState()))
+
+  const onUpdateStore = () => {
+    const currentSelectedStore = selector(store.getState())
+    const prevSelectedStore = selectedStore
+
+    const shouldUpdate = isEqual ? !isEqual(currentSelectedStore, prevSelectedStore) : true
+
+    if(shouldUpdate) {
+      setSelectedStore(currentSelectedStore)
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(onUpdateStore)
+    return () => unsubscribe()
+  }, [selectedStore])
+
+  return selectedStore
+}
+const useDispatch = () => {
+  const { store } = React.useContext(Context)
+  return store.dispatch
+}
+
+const Provider = ({ store, children }) => {
+  return <Context.Provider value={{ store }}>{children}</Context.Provider>
+}
+
+const UPDATE_COUNTER = 'UPDATE_COUNTER'
+const CHANGE_STEP_SIZE = 'CHANGE_STEP_SIZE'
+
+const updateCounter = value => ({
+  type: UPDATE_COUNTER,
+  payload: value,
+})
+
+const changeStepSize = value => ({
+  type: CHANGE_STEP_SIZE,
+  payload: value,
+})
+
+
+const defaultState = {
+  counter: 1,
+  stepSize: 1,
+}
+
+const reducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case UPDATE_COUNTER:
+      return { ...state, counter: state.counter + action.payload }
+    case CHANGE_STEP_SIZE:
+      return { ...state, stepSize: action.payload }
+    default:
+      return state
+  }
+}
+
+const Counter = () => {
+  const counter = useSelector(state => state.counter)
+  const stepSize = useSelector(state => state.stepSize, (current, prev) => current === prev)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <button onClick={() => dispatch(updateCounter(-stepSize))}>-</button>
+      <span> {counter} </span>
+      <button onClick={() => dispatch(updateCounter(stepSize))}>+</button>
+    </div>
+  )
+}
+
+const Step = () => {
+  const stepSize = useSelector(state => state.stepSize, (current, prev) => current === prev)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>Значение счётчика должно увеличиваться или уменьшаться на заданную величину шага</div>
+      <div>Текущая величина шага: {stepSize}</div>
+      <input
+        type="range"
+        min="1"
+        max="5"
+        value={stepSize}
+        onChange={({ target }) => dispatch(changeStepSize(Number(target.value)))}
+      />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={createStore(reducer, defaultState)}>
+    <Step />
+    <Counter />
+  </Provider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
